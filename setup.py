177268e2eb2210fa36a29df95763f40523a3f6e5
chenjingli <git@git.xiaojukeji.com:chenjingli/dcloud-auto-ui.git>
2018-05-29 15:05:49 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ NAME = "flog4p"
 DESCRIPTION = "Lightweight python log frame"
 AUTHOR = "Allen Chen"
 AUTHOR_EMAIL = "1010584905@qq.com"
-URL = "git@github.com:xA11en/flog4p.git"
+URL = "https://github.com/xA11en/flog4p.git"
 VERSION = __import__(PACKAGE).__version__
 
 setup(
@@ -16,8 +16,15 @@ setup(
     # long_description=read("README.md"),
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
-    # "Apache License, Version 2.0",
+    license="Apache License, Version 2.0",
     url=URL,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
     packages=["flog4p"],
     zip_safe=False,
 )
