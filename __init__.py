e42de8e34bd783b0530bd068f70c5563063973f9
chenjingli <git@git.xiaojukeji.com:chenjingli/dcloud-auto-ui.git>
2018-06-01 16:26:33 +0800

    update init.py

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ def help():
     print 'isFile: True,False; False:只输出到控制台，True:输出到文件并打印到控制台，只提供 info 和 error 级别'
     print 'path: 如果  isFile 为 True 时 path 要求填写日志到绝对路径 如上'
     print '打印用例：不带参数,带参数'
-    print 'logger1.info("test format logs")'
-    print 'logger1.info("test format logs first params is {},sencond  params is {}", "one",222)'
+    print 'logger.info("test format logs")'
+    print 'logger.info("test format logs first params is {},sencond  params is {}", "one",222)'
     print '输出带参数带样例格式为：[INFO] 2018-05-25 18:11:52.343942 [MainThread] test format logs first params is one,sencond  params is 222'
 
